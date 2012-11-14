91f767daeda3a13aadff39315769efb36d14a591
Mathias Gebhardt (LT) <gebhardtmathias@aol.com>
2012-11-14 19:08:54 +0100

    Searches manifest file and also loads component's name from this file.

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-from xml.dom.minidom import parseString
+from xml.dom.minidom import parse
 import zipfile
 import os
 import sys
@@ -6,31 +6,45 @@ import sys
 # Change to components root directory
 os.chdir('..')
 
-# TODO: method to get manifest file
-file = open('osbit.xml', 'r')
-data = file.read()
-file.close()
+found = False
 
-# create XML dom 
-dom = parseString(data)
+# search manifest file
+for file in os.listdir('.'):
+	# must be a xml file
+	if '.xml' != file[-4:]:
+		continue
+	
+	# open file and lock in it
+	# create XML dom 
+	manifest = parse(file)
+	
+	# root node must be extension
+	if 'extension' == manifest.firstChild.nodeName:
+		found = True
+		break;
+
+if not found:
+	print 'Manifest not found!'
+	sys.exit(-1)
+
+# get name
+name = manifest.getElementsByTagName('name')[0].firstChild.data.lower()
 
 # get version
-version = dom.getElementsByTagName('version')[0].firstChild.data
-# print version
+version = manifest.getElementsByTagName('version')[0].firstChild.data
 
 # get folders
 folders = []
 # get frontend and backend folder
-for folder in dom.getElementsByTagName('files'):
+for folder in manifest.getElementsByTagName('files'):
 	folders.append(folder.attributes['folder'].value)
 # get folder for media
-folders.append(dom.getElementsByTagName('media')[0].attributes['folder'].value)
+folders.append(manifest.getElementsByTagName('media')[0].attributes['folder'].value)
 # get folder for installer language
 folders.append('language')
 
 # create file name for archive
-# TODo: use component Name here
-zipFileName = os.path.join('releases', 'com_osbit-{0}.zip'.format(version))
+zipFileName = os.path.join('releases', '{}-{}.zip'.format(name, version))
 #print zipFileName
 
 # open archive
@@ -59,4 +73,4 @@ finally:
 	# every thing added, than close
 	installer.close()
 	
-print 'successfully created {}', zipFileName
+print 'successfully created {}'.format(zipFileName)
