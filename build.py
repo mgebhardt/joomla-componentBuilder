51dc5dda6d1b7c94e352bcfa8da0f9605ea917d2
Mathias Gebhardt (NB) <gebhardtmathias@aol.com>
2012-11-16 11:20:39 +0100

    Add update server support.
    Released version 1.1.0

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 #===============================================================================
-# @version:		1.0.0
+# @version:		1.1.0
 # @summary:		This python script creates install archives for Joomla componentes
 # @copyright:	(C) 2012 Mathias Gebhardt
 # @license:		GNU General Public License version 2 or later; see LICENSE.txt
@@ -9,9 +9,14 @@ from xml.dom.minidom import parse
 import zipfile
 import os
 import sys
+import urllib2
 
 # Change to components root directory
 os.chdir('..')
+
+#===============================================================================
+# Search manifest and get some parameters
+#===============================================================================
 
 found = False
 
@@ -40,6 +45,13 @@ name = manifest.getElementsByTagName('name')[0].firstChild.data.lower()
 # get version
 version = manifest.getElementsByTagName('version')[0].firstChild.data
 
+# get updateServer
+if len(manifest.getElementsByTagName('updateservers')) > 0:
+	# if update section exsits
+	updateServer = manifest.getElementsByTagName('server')[0].firstChild.data
+else:
+	updateServer = False
+
 # get folders
 folders = []
 # get frontend and backend folder
@@ -49,6 +61,10 @@ for folder in manifest.getElementsByTagName('files'):
 folders.append(manifest.getElementsByTagName('media')[0].attributes['folder'].value)
 # get folder for installer language
 folders.append('language')
+
+#===============================================================================
+# Create zip archive
+#===============================================================================
 
 # create file name for archive
 zipFileName = os.path.join('releases', '{}-{}.zip'.format(name, version))
@@ -81,3 +97,41 @@ finally:
 	installer.close()
 	
 print 'successfully created {}'.format(zipFileName)
+
+#===============================================================================
+# Get update server file and add new update section
+#===============================================================================
+
+if updateServer == False:
+	# no update server found
+	print 'Skipping update server'
+	sys.exit(0)
+
+file = urllib2.urlopen(updateServer)
+dom = parse(file)
+
+# get first update and copy it
+# first and last child are \n
+# so you you have to search for update
+# TODO: search latest update
+lastUpdate = dom.getElementsByTagName('update')[0]
+update = lastUpdate.cloneNode(True)
+
+# update version
+update.getElementsByTagName('version')[0].firstChild.data = version
+# update download url
+update.getElementsByTagName('downloadurl')[0].firstChild.data = \
+    update.getElementsByTagName('downloadurl')[0].firstChild.data.rsplit \
+        ('/', 1)[0] + '/' + '{}-{}.zip'.format(name, version)
+# add new tag at first position
+dom.getElementsByTagName('updates')[0].insertBefore(update, lastUpdate)
+
+# get file name
+updateXML = os.path.join('releases', updateServer.rsplit('/', 1)[1])
+
+# Write dom to file
+xmlFile = open(updateXML, 'w')
+dom.writexml(xmlFile)
+xmlFile.close()
+
+print 'successfully created {}'.format(updateXML)
